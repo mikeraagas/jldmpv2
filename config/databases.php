ea2b85c9bc0daeb80e5f1589231771015c1ccd94
Michael Raagas <michael.dpoint@gmail.com>
2014-04-10 19:46:49 +0800

    restore config and uploads

---
+++ config/databases.php
@@ -0,0 +1,9 @@
+<?php //-->
+return array (
+	'default' => array(
+		'host' 	=> '127.0.0.1',
+		'name' 	=> 'jldmp',
+		'user' 	=> 'root',
+		'pass' 	=> 'root',
+		'type' 	=> 'mysql',
+		'default' => true));
---
+++ config/pages.php
@@ -0,0 +1,11 @@
+<?php //-->
+return array(
+	'/ministry/*/members' 		=> 'Control_Page_Ministry_Members',
+	'/ministry/*/events' 		=> 'Control_Page_Ministry_Events',
+	'/ministry/*/member/add' 	=> 'Control_Page_Ministry_Member_Add',
+	'/ministry/*/member/edit' 	=> 'Control_Page_Ministry_Member_Edit',
+	'/ministry/*/member/view' 	=> 'Control_Page_Ministry_Member_View',
+	'/ministry/*/event/add' 	=> 'Control_Page_Ministry_Event_Add',
+	'/ministry/*/event/edit' 	=> 'Control_Page_Ministry_Event_Edit',
+	'/ministry/*/event/view' 	=> 'Control_Page_Ministry_Event_View'
+);
